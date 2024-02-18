1bc783b4a37a17b0cefd5a1d69ef69830c5ec807
benjaminfarias313 <benjaminfarias313@gmail.com>
2024-02-17 19:27:24 -0800

    added default favicon added logic to change favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
 	import { AppShell } from '@skeletonlabs/skeleton';
 	import '../app.postcss';
 	import { ERROR_WAIT_TIME, APPLICATION_NAME } from 'constants/constants';
@@ -8,6 +9,15 @@
 
 	let invalidFileInput: boolean = false;
 	let appTheme: 'light' | 'dark' = 'dark';
+	let favicon = writable('favicon-dark.png');
+
+	const changeFavicon = () => {
+		if (appTheme === 'light') {
+			favicon.set('favicon-light.png');
+		} else {
+			favicon.set('favicon-dark.png');
+		}
+	};
 
 	const handleWrongFileInput = () => {
 		invalidFileInput = true;
@@ -21,10 +31,21 @@
 		} else {
 			appTheme = 'light';
 		}
+		changeFavicon(); // Call changeFavicon when appTheme changes
 	};
+
+	// Watch for changes in the favicon and update the document's favicon
+	$: favicon = favicon; // This line triggers reactivity whenever $favicon changes
+	favicon.subscribe((newFavicon) => {
+		const faviconLink = document.querySelector('link[rel="icon"]');
+		if (faviconLink) {
+			faviconLink.setAttribute('href', newFavicon);
+		}
+	});
 
 	onMount(() => {
 		document.title = APPLICATION_NAME;
+		changeFavicon();
 	});
 </script>
 
