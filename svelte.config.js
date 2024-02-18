ae564440ddd2201b163f158bf037dea1a31271ec
benjaminfarias313 <benjaminfarias313@gmail.com>
2024-02-18 00:29:51 -0800

    updated svelte.config

--- svelte.config.js
+++ svelte.config.js
@@ -10,9 +10,7 @@ const config = {
 	preprocess: [vitePreprocess()],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		// Pass the adapter configuration directly to the adapter property
 		adapter: adapter(),
 		files: {
 			assets: path.resolve('./public')
@@ -20,7 +18,8 @@ const config = {
 		alias: {
 			constants: 'src/constants',
 			routes: 'src/routes',
-			abstract: 'src/abstract'
+			abstract: 'src/abstract',
+			models: 'src/models'
 		}
 	}
 };
