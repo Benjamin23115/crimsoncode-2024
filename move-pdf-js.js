6aa1e79155b83e054c1742af311c5307848fb85c
benjaminfarias313 <benjaminfarias313@gmail.com>
2024-02-17 20:43:46 -0800

    add script for moving pdf-js add npm script to move pdf-js add pdf-js to gitignore

---
+++ move-pdf-js.js
@@ -0,0 +1,34 @@
+import fs from 'fs';
+import path from 'path';
+import { execSync } from 'child_process';
+const source = path.join('./node_modules/@pdftron/pdfjs-express/public/');
+const destination = path.join('./src/pdf-js'); // Adjust if needed
+
+async function copyFiles() {
+	try {
+		// Check if destination directory exists
+		if (!(await fs.existsSync(destination))) {
+			await fs.mkdirSync(destination, { recursive: true });
+		}
+
+		// Use system-specific command based on platform
+		const command = getCommandForPlatform();
+		await execSync(command, { stdio: 'inherit' });
+
+		console.log(`Copied files from ${source} to ${destination}`);
+	} catch (error) {
+		console.error(`Error copying files: ${error.message}`);
+	}
+}
+
+function getCommandForPlatform() {
+	if (process.platform === 'win32') {
+		return `copy "${source}" "${destination}" /y`;
+	} else if (process.platform === 'darwin') {
+		return `cp -r "${source}" "${destination}"`;
+	} else {
+		return `cp -r "${source}" "${destination}"`;
+	}
+}
+
+copyFiles();
