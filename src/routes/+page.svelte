d78650191ea804961d445a3f70d38a601be6290e
benjaminfarias313 <benjaminfarias313@gmail.com>
2024-02-18 00:30:36 -0800

    added initial logic for processing pdf with library

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,48 @@
 <script lang="ts">
-	import { createEventDispatcher } from 'svelte';
 	import { FileDropzone } from '@skeletonlabs/skeleton';
 	import Alert from 'abstract/Alert/Alert.svelte';
-	import PdfViewer from "routes/PdfViewer/PdfViewer.svelte"
-	import { ALERT_WAIT_TIME } from 'constants/constants';
+	import PdfViewer from 'routes/PdfViewer/PdfViewer.svelte';
+	import { ALERT_WAIT_TIME, APPLICATION_NAME } from 'constants/constants';
+	import { readFileAsUint8Array } from 'models/models';
 
 	let invalidFileInput: boolean = false;
 	let validFileInput: boolean = false;
-
+	let validAlertCheck: boolean = false;
+	let fileExtensionCheck: boolean = false;
+	let fileAsUInt8Array: Uint8Array;
 	let files: FileList;
-	let fileExtensionCheck: boolean;
 
-	const onChangeHandler = (e: Event) => {
+	const onChangeHandler = async (e: Event) => {
 		fileExtensionCheck = files[0].name.endsWith('.pdf');
 		if (fileExtensionCheck) {
 			validFileInput = true;
 			setTimeout(() => {
 				validFileInput = false;
 			}, ALERT_WAIT_TIME);
-			// send pdf file to OCR
-		} else {
-			invalidFileInput = true;
-			setTimeout(() => {
-				invalidFileInput = false;
-			}, ALERT_WAIT_TIME);
+			if (files[0]) {
+				try {
+					fileAsUInt8Array = await readFileAsUint8Array(files[0]);
+					validAlertCheck = true;
+				} catch (error) {
+					console.error('Error reading file:', error);
+					invalidFileInput = true;
+					setTimeout(() => {
+						invalidFileInput = false;
+					}, ALERT_WAIT_TIME);
+				}
+			} else {
+				invalidFileInput = true;
+				setTimeout(() => {
+					invalidFileInput = false;
+				}, ALERT_WAIT_TIME);
+			}
 		}
 	};
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center flex-col gap-8">
 	<div class="space-y-10 text-center flex flex-col items-center">
-		<h2 class="h2">Welcome to Tuff2DBug</h2>
+		<h2 class="h2">Welcome to {APPLICATION_NAME}</h2>
 		<FileDropzone name="files" bind:files on:change={onChangeHandler} rounded="rounded">
 			<svelte:fragment slot="lead"
 				><span class="material-symbols-outlined"> upload_file </span></svelte:fragment
@@ -51,6 +63,9 @@
 			variant={'variant-filled-success'}
 			icon={'done'}
 		/>
+	{/if}
+	{#if !validFileInput && validAlertCheck}
+		<PdfViewer pdf={fileAsUInt8Array} />
 	{/if}
 </div>
 
